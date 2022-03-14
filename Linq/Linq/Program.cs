619f553e0df79120fd323ad204f53bef01944caa
JoniMujica <jonimujica3@hotmail.com>
2022-03-14 14:05:04 -0300

    linq

---
+++ Linq/Linq/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Linq
+{
+    public class Program
+    {
+        delegate void MiDelegado(string mensaje);
+        static void Main(string[] args)
+        {
+            MiDelegado mensajeador = new MiDelegado(EscribirMensaje);
+            mensajeador("Hello Word!");
+            //Console.WriteLine("Hello World!");
+        }
+        public static void EscribirMensaje(string mensaje)
+        {
+            Console.WriteLine(mensaje);
+        }
+    }
+}
