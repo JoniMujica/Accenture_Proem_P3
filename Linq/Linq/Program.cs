71bab779cd58df789d979e4d09423dd75d1dfe5c
JoniMujica <jonimujica3@hotmail.com>
2022-03-14 14:20:59 -0300

    delegados func,action,predicate

--- Linq/Linq/Program.cs
+++ Linq/Linq/Program.cs
@@ -4,20 +4,23 @@ namespace Linq
 {
     public class Program
     {
-        delegate void MiDelegado(string mensaje);
+       // delegate void MiDelegado(string mensaje);
         static void Main(string[] args)
         {
-            MiDelegado mensajeador = new MiDelegado(EscribirMensaje);
-            mensajeador("Hello Word!");
+            //MiDelegado mensajeador = new MiDelegado(EscribirMensaje);
+            //mensajeador("Hello Word!");
             //Console.WriteLine("Hello World!");
+
+            Action<int> action = (num) => Console.WriteLine(num);  //el parametro num debe ir obligado ya que en Action<> declaramos un int
+            action(1);
         }
-        public static void EscribirMensaje(string mensaje)
+        /*public static void EscribirMensaje(string mensaje)
         {
             Console.WriteLine(mensaje);
         }
         public static void EscribirMensaje2(int numero)
         {
             Console.WriteLine(numero);
-        }
+        }*/
     }
 }
