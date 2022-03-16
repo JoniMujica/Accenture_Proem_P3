b8efc0463140d19d2da42a98102e5879da84a41d
JoniMujica <jonimujica3@hotmail.com>
2022-03-16 11:06:26 -0300

    Configuramos el DB context en MyDBContext.cs y Creamos los modelos para referenciar en la DB

---
+++ EntityFramework/CEntityFrameworkCodeFirst/Entidades/Data/MyDBContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Data
+{
+    //clase de contexto DB
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext()
+        {
+
+        }
+        public DbSet<Persona> Persona { get; set; } //esto se setea en base a la class persona que es padre de Profesor y Alumno
+        public MyDBContext(DbContextOptions<MyDBContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=localhost;Database=MyDB;Trusted_Connection=True;");
+        }
+
+    }
+}
