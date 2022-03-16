e41def5cd4d8471dff6e01ea218f9d3afccba426
JoniMujica <jonimujica3@hotmail.com>
2022-03-16 13:33:46 -0300

    fin entityFramework

--- EntityFramework/CEntityFrameworkCodeFirst/CEntityFrameworkCodeFirst/Program.cs
+++ EntityFramework/CEntityFrameworkCodeFirst/CEntityFrameworkCodeFirst/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using Entidades.Models;
+using Entidades.Data;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CEntityFrameworkCodeFirst
 {
-    internal class Program
+    public class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Alumno> alumnos = new List<Alumno>
+            {
+                new Alumno{Nombre = "Pepe",Apellido = "Gonzales",Calificacion = 10,Edad = 20},
+                new Alumno{Nombre = "Luis",Apellido="Fernandez",Calificacion=10,Edad=30},
+                new Alumno{Nombre = "Jesus",Apellido="Lopez",Calificacion=10,Edad=30},
+                new Alumno{Nombre = "Erik",Apellido="Messi",Calificacion=10,Edad=25},
+            };
+            using (MyDBContext db = new MyDBContext())
+            {
+                alumnos.ForEach(alumn => db.Alumnos.Add(alumn).State = Microsoft.EntityFrameworkCore.EntityState.Added);
+                db.SaveChanges();
+            }
         }
     }
 }
--- EntityFramework/CEntityFrameworkCodeFirst/Entidades/Models/Alumno.cs
+++ EntityFramework/CEntityFrameworkCodeFirst/Entidades/Models/Alumno.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace Entidades.Models
 {
     public sealed class Alumno : Persona
     {
+        /*[Key] //data notation, es para marcar este campo como si fuera ID en nuestra DB   
+        public string Algo { get; set; }*/
         public int Calificacion { get; set; }
     }
 }
