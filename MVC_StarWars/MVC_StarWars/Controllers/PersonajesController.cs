c7947abf4a9aaa533ace2e5df452ba16c94b229e
JoniMujica <jonimujica3@hotmail.com>
2022-03-22 20:36:07 -0300

    Creamos MVC y agregamos modelo de prueba + vista enrutador

---
+++ MVC_StarWars/MVC_StarWars/Controllers/PersonajesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_StarWars.Controllers
+{
+    public class PersonajesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC_StarWars/MVC_StarWars/Models/Personaje.cs
@@ -0,0 +1,6 @@
+﻿namespace MVC_StarWars.Models
+{
+    public class Personaje
+    {
+    }
+}
