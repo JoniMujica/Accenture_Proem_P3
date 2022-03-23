e4f4e2b8e84491cbb1f96c904ab1192a38758bd0
JoniMujica <jonimujica3@hotmail.com>
2022-03-22 21:21:47 -0300

    arreglamos contructores de la DB

---
+++ MVC_StarWars/MVC_StarWars/Models/StarwarsDb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVC_StarWars.Models
+{
+    public class StarwarsDb : DbContext
+    {
+        public DbSet<Personaje> Personajes { get; set; }
+
+        public StarwarsDb()
+        {
+
+        }
+        public StarwarsDb(DbContextOptions<StarwarsDb> options):base(options)
+        {
+
+        }
+        /*protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+           // options.UseInMemoryDatabase("Startwars");
+        }*/
+    }
+}
