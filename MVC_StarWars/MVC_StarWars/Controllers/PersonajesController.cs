105821fcb8710042754748305b615c15538f332f
JoniMujica <jonimujica3@hotmail.com>
2022-03-23 12:25:58 -0300

    agregamos el metodo DELETE, instalamos entity sql y tools

--- MVC_StarWars/MVC_StarWars/Controllers/PersonajesController.cs
+++ MVC_StarWars/MVC_StarWars/Controllers/PersonajesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MVC_StarWars.Models;
 using System;
 
@@ -12,11 +13,28 @@ namespace MVC_StarWars.Controllers
             this.context = context;
         }
 
-        public IActionResult Listado(string nombre)
+        public IActionResult Listado()
         {
             var model = new PersonajesListadoModel();
             model.Listado = context.Personajes.ToList();
             return View(model); //ESTO SE LO PASAMOS COMO PARAMETRO A  LA VISTA
+        }
+        public IActionResult Eliminar(string id)
+        {
+            int Id = 0;
+            int.TryParse(id, out Id);
+            if (Id < 1)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                Personaje p = context.Personajes.Where((p) => p.Id == Id).First();
+
+                context.Personajes.Remove(p).State = EntityState.Deleted;
+                context.SaveChanges();
+                return RedirectToAction("Listado");
+            }
         }
 
         [HttpPost]
