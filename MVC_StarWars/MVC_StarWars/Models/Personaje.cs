fc42fc9402afb330092fcd825e6bb51924c1f898
JoniMujica <jonimujica3@hotmail.com>
2022-03-23 15:35:00 -0300

    le agrego una nuevo propiedad a mi modelo y realizo una migracion + updateDB

--- MVC_StarWars/MVC_StarWars/Models/Personaje.cs
+++ MVC_StarWars/MVC_StarWars/Models/Personaje.cs
@@ -7,5 +7,6 @@ namespace MVC_StarWars.Models
         public int Id { get; set; }
         [Required(ErrorMessage = "El nombre es obligatorio")]
         public string Nombre { get; set; }
+        public int TipoId { get; set; } //aca modifique la modelo de la DB, si o si tengo que ahce un migration
     }
 }
