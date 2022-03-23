90f54b46a65d33735bf522968445f489c2ba47ef
JoniMujica <jonimujica3@hotmail.com>
2022-03-23 15:45:05 -0300

    le agrego Display la cual modificara el label del frontend

--- MVC_StarWars/MVC_StarWars/Models/Personaje.cs
+++ MVC_StarWars/MVC_StarWars/Models/Personaje.cs
@@ -7,6 +7,7 @@ namespace MVC_StarWars.Models
         public int Id { get; set; }
         [Required(ErrorMessage = "El nombre es obligatorio")]
         public string Nombre { get; set; }
+        [Display(Name = "Tipo de personaje")] //esto nos va a cambiar el label del dropdown de la vista Editar
         public int TipoId { get; set; } //aca modifique la modelo de la DB, si o si tengo que ahce un migration
     }
 }
