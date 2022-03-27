6b4e7c840d516d6bc86a5d38f2ed9c086be26a1d
JoniMujica <jonimujica3@hotmail.com>
2022-03-27 11:27:17 -0300

    archivos de migracion

---
+++ MVC_StarWars/MVC_StarWars/Migrations/20220327141700_Misiones.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVC_StarWars.Migrations
+{
+    public partial class Misiones : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Misiones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PersonajeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Misiones", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Misiones_Personajes_PersonajeId",
+                        column: x => x.PersonajeId,
+                        principalTable: "Personajes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Misiones_PersonajeId",
+                table: "Misiones",
+                column: "PersonajeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Misiones");
+        }
+    }
+}
--- MVC_StarWars/MVC_StarWars/Models/StarwarsDb.cs
+++ MVC_StarWars/MVC_StarWars/Models/StarwarsDb.cs
@@ -5,6 +5,7 @@ namespace MVC_StarWars.Models
     public class StarwarsDb : DbContext
     {
         public DbSet<Personaje> Personajes { get; set; }
+        public DbSet<Mision> Misiones { get; set; }
 
         public StarwarsDb()
         {
@@ -14,6 +15,12 @@ namespace MVC_StarWars.Models
         {
 
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Mision>().ToTable("TablaDeMision");
+            //modelBuilder.Entity<Mision>().HasOne(x => x.Personaje);
+        }
+
         /*protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
            // options.UseInMemoryDatabase("Startwars");
