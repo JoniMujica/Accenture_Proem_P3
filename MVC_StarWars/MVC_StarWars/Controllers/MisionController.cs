f8b636ca9fa41e3d4631efc41bfea0147a1b8370
JoniMujica <jonimujica3@hotmail.com>
2022-03-27 11:26:32 -0300

    agregamos un controlador la cuañ hara query join de mision + personaje

---
+++ MVC_StarWars/MVC_StarWars/Controllers/MisionController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC_StarWars.Models;
+
+namespace MVC_StarWars.Controllers
+{
+    public class MisionController : Controller
+    {
+        public StarwarsDb context { get; set; }
+        public MisionController(StarwarsDb context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.Lista = context.Misiones.Include(x=> x.Personaje).ToList(); //con esto me va a traer todas las misiones con sus personajes (por esoel include)
+            return View();
+        }
+        public IActionResult Editar(string id)
+        {
+            return View();
+        }
+    }
+}
