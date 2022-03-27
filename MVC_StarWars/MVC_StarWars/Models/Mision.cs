7096ac8e44289bb6b8ee00e915ff16840e504f40
JoniMujica <jonimujica3@hotmail.com>
2022-03-27 10:54:59 -0300

    agregamos un nuevo modelo para relacionar personajes

---
+++ MVC_StarWars/MVC_StarWars/Models/Mision.cs
@@ -0,0 +1,12 @@
+﻿namespace MVC_StarWars.Models
+{
+    public class Mision
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Titulo { get; set; }
+        public int PersonajeId { get; set;} //esto es para agregar en la tabla la ID del personaje
+        public Personaje Personaje { get; set; } //esto es para que entity framework relacione el valor de PersonajeId
+
+    }
+}
