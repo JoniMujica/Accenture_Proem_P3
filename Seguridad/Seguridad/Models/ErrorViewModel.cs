692e919941dd2c64674ab5a0bdb44ac3c79e4c5a
JoniMujica <jonimujica3@hotmail.com>
2022-04-01 15:17:14 -0300

    Seguridad Auth .NET

---
+++ Seguridad/Seguridad/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Seguridad.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
