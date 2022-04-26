d977ddde29a16ce242ed588b757407c4a7d01b2a
JoniMujica <jonimujica3@hotmail.com>
2022-04-26 14:49:02 -0300

    trabajo con el mismo projecto p2

---
+++ Seguridad_p2/Seguridad/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Seguridad.Models;
+using System.Diagnostics;
+using System.Security.Claims;
+
+namespace Seguridad.Controllers
+{
+    [Authorize] //esto hace que requiera que este autorizado para acceder al Home
+
+    [Authorize(Policy = "SoloClientes")] //este Claim, se agrega en Program.cs
+    //[Authorize(Policy = "Remotos")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [AllowAnonymous] //esto me permite ingresar a este metodo/vista sin autorizacion
+        public IActionResult Index()
+        {
+            /*var claim = new Claim("Admin", "false"); //agrega "roles" a los usuarios
+            if (User.Claims.Any(x => x.Type == "Admin")*/ //con eso verifico si el usuario tiene un rol X
+
+
+            /*if (User.Identity.IsAuthenticated) //valida si esta logueado
+            {
+                return View();
+            }*/
+            ViewBag.Valor1 = "valor1";
+            TempData["valor2"] = "valor2";
+
+            return View();
+            
+        }
+
+        public IActionResult Privacy()
+        {
+            var valor2 = TempData["valor2"];
+            var valor1 = ViewBag.Valor1;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
