13c25cd9f36fcdd31d52f6944b5ebc18dcba8568
JoniMujica <jonimujica3@hotmail.com>
2022-04-27 11:09:31 -0300

    metodo de modificacion de tablas 1

--- WebAPI/WebAPI/Models/DataContext.cs
+++ WebAPI/WebAPI/Models/DataContext.cs
@@ -9,6 +9,14 @@ namespace WebAPI.Models
         {
 
         }
+        //esto es para cambiar el nombre de la tabla de la db
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Articulo>().ToTable("LosArticulos");
+            modelBuilder.Entity<Articulo>().Property(p => p.Nombre).HasMaxLength(60);
+            modelBuilder.Entity<Articulo>().HasIndex(p => p.Nombre);
+        }
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
