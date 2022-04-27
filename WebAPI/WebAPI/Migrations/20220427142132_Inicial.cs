7c9399872e0fe487e14b87ce9531b68ac93bccc5
JoniMujica <jonimujica3@hotmail.com>
2022-04-27 11:51:25 -0300

    f

---
+++ WebAPI/WebAPI/Migrations/20220427142132_Inicial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LosArticulos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Precio = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LosArticulos", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LosArticulos_Nombre",
+                table: "LosArticulos",
+                column: "Nombre");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LosArticulos");
+        }
+    }
+}
