bd820c503e25ff7d3568b7762b7da2099c652ac6
JoniMujica <jonimujica3@hotmail.com>
2022-04-27 11:08:34 -0300

    creamos y configuramos la API + Modelo

---
+++ WebAPI/WebAPI/Models/Articulo.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI.Models
+{
+    public class Articulo
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
---
+++ WebAPI/WebAPI/Models/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Models
+{
+    public class DataContext :DbContext
+    {
+        public DbSet<Articulo> Articulos { get; set;}
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
