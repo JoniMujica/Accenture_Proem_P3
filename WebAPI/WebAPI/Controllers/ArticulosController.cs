b10c7cdc49a0efbab45db5a95a3996c23d76d205
JoniMujica <jonimujica3@hotmail.com>
2022-04-27 11:48:43 -0300

    agregamos controlador de articulos

---
+++ WebAPI/WebAPI/Controllers/ArticulosController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticulosController : ControllerBase
+    {
+        [HttpGet]
+        public Articulo Get()
+        {
+            return new Articulo();
+        }
+        [HttpPost]
+        public Articulo Post()
+        {
+            return new Articulo { Id = 1 };
+        }
+    }
+}
