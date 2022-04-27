820cf41e864e542f487ac0da7d6fe84d564ca629
JoniMujica <jonimujica3@hotmail.com>
2022-04-27 11:50:10 -0300

    creamos modelo de configuracion de tabla

---
+++ WebAPI/WebAPI/Models/Configuration/ArticuloConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+namespace WebAPI.Models.Configuration
+{
+    public class ArticuloConfiguration : IEntityTypeConfiguration<Articulo>
+    {
+        public void Configure(EntityTypeBuilder<Articulo> builder)
+        {
+            builder.ToTable("LosArticulos");
+            builder.Property(p => p.Nombre).HasMaxLength(50);
+            builder.HasIndex(p => p.Nombre);
+        }
+    }
+}
