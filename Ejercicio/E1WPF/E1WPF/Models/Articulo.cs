4fd12a3a6b1c69f6a55775308d57b5ad8961d071
JoniMujica <jonimujica3@hotmail.com>
2022-04-27 19:35:19 -0300

    Ejercicio WPF, creacion de form + vinculo de objeto

---
+++ Ejercicio/E1WPF/E1WPF/Models/Articulo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E1WPF.Models
+{
+    public class Articulo
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
