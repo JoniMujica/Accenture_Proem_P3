e863d1f1f27b5b6f1281f73f722cc49d4e128917
JoniMujica <jonimujica3@hotmail.com>
2022-04-30 16:27:11 -0300

    realizamos la creacion de archivos excel con NPOI y sus tablas de la API

--- WebAPI/WebAPI/Controllers/ArticulosController.cs
+++ WebAPI/WebAPI/Controllers/ArticulosController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
 using WebAPI.Models;
 
 namespace WebAPI.Controllers
@@ -66,6 +68,39 @@ namespace WebAPI.Controllers
             await dataContext.SaveChangesAsync();
             return NoContent();
         }
-        
+
+
+        [HttpGet]
+        [Route("Export")]
+        public async Task<IActionResult> Export()
+        {
+            var articulos = await dataContext.Articulos.ToListAsync();
+            IWorkbook worlbook = new XSSFWorkbook();
+            ISheet sheet1 = worlbook.CreateSheet("Articulos");
+            IRow row = sheet1.CreateRow(0);
+            row.CreateCell(0).SetCellValue("Codigo");
+            row.CreateCell(1).SetCellValue("Nombre");
+            row.CreateCell(2).SetCellValue("Precio");
+            int rowNumber = 1;
+            foreach (var item in articulos)
+            {
+                row = sheet1.CreateRow(rowNumber);
+                row.CreateCell(0).SetCellValue(item.Id);
+                row.CreateCell(1).SetCellValue(item.Nombre);
+                row.CreateCell(2).SetCellValue((double)item.Precio);
+                rowNumber++;
+            }
+            var st = new MemoryStream();
+            worlbook.Write(st);
+            var st2 = new MemoryStream();
+            var array = st.ToArray();
+            st2.Write(array,0,array.Length);
+            //await st.FlushAsync();
+            st2.Position = 0;
+            //st.Close();
+            return File(st2, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet","Reporte.xlsx");
+        }
+
     }
 }
+ 
