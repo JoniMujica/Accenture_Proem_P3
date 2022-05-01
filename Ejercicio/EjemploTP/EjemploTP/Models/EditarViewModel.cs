6af636a7ba28f840a97600288d1ccba2516e21a6
JoniMujica <jonimujica3@hotmail.com>
2022-05-01 14:31:18 -0300

    agregamos funcionalidades para listas estaticas (max 5)

---
+++ Ejercicio/EjemploTP/EjemploTP/Models/EditarViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace EjemploTP.Models;
+
+public class EditarViewModel
+{
+    public Trago Tragos { get; set; }
+    //public List<Ingrediente> Ingredientes { get; set;}
+    public Ingrediente Item1 { get; set; }
+    public Ingrediente Item2 { get; set; }
+    public Ingrediente Item3 { get; set; }
+    public Ingrediente Item4 { get; set; }
+    public Ingrediente Item5 { get; set; }
+
+}
+
